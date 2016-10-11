020a8c6d9764eb460b3b358bf788bdd7533e7c52
enotocode <enotocode@yandex.ru>
2016-10-11 13:07:09 +0800

    simplified markSameStr()

--- app/ViewHelper.php
+++ app/ViewHelper.php
@@ -2,16 +2,12 @@
 namespace app;
 class ViewHelper
 {
-    public static function  highlightSymbols($str, $mask, $htmlTag = "<strong><\strong>")
-    {        
-        $mask = preg_replace (
-                                array("![^a-zA-ZА-Яа-я0-9\\s]!ui", "! +!ui"),
-                                array("", " "),
-                                $mask
-                                );
-        $mask = "/" . $mask . "/iu";
-        preg_match("!(<\\w+>)(<\\/\\w+>)!ui", $htmlTag, $htmlTags);
-        return preg_replace($mask, "{$htmlTags[1]}$0{$htmlTags[2]}", $str);
+    public static function  markSameStr($str, $mask, $htmlTag = "<mark>%s</mark>")
+    {
+        if ($str == $mask) {
+            return sprintf($htmlTag, $str);
+        }      
+        return $str;
     }
     public static function html($text)
     {
