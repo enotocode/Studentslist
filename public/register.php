1027e14ac4131f768affb8eca30812243330ba71
enotocode <enotocode@yandex.ru>
2016-10-11 13:10:57 +0800

    add supporting Authorization class add supporting View class

--- public/register.php
+++ public/register.php
@@ -1,74 +1,50 @@
 <?php
-require_once '\..\app\init.php';
+require_once  (__DIR__ . '/../app/init.php');
 
 // Проверяем зарегистрирован ли пользователь, если да, то получаем его id
-$autorization = new app\Autorization($adg);
-$userId = NULL;
-if (isset($_COOKIE['userPassword']) && ($autorization->getUserId($_COOKIE['userPassword']))) {
-    $userId = $autorization->getUserId($_COOKIE['userPassword']);    
-} 
-  
+$autorization = new app\Authorization($adg);
+$autorization->authorize();
+$userId = $autorization->getUserId();
+
+// Создаем объект Абитуриент
+$abiturient = new app\Abiturient();
+
 // Проверяем была ли отправлена форма
 $values = "";
-$errors = array();
+$errors = FALSE;
 if (!empty($_POST)) {
-    // Необходимые значения формы для создания объекта Abiturient
-    $abiturientValues = array(
-        "name",
-        "lastName",
-        "gender",
-        "groupNum",
-        "email",
-        "egePoints",
-        "dateOfBirth",
-        "registry"
-    );
-    // Выбираем из $_POST нужные параметры
-    $values = array_intersect_key($_POST, array_flip($abiturientValues));
-    // Проводим магический ритуал очищения значений параметров полученных из формы
-    foreach ($values as $valueName=>$value) {
-        $value = trim($value);
-        // Каким-то образом экранируем спецсимволы
-    }   
-    // Создаем объект Абитуриент
-    $abiturient = new app\Abiturient ($values);
+    $formData = array_map(
+                          function ($a) {return trim($a);},
+                          $_POST
+                          );
+    // Обновляем студента
+    $abiturient->updateValues($formData);
     // Отправляем его на валидацию
     $abiturientValidator = new app\AbiturientValidator();
-    $errors = $abiturientValidator->validate($abiturient);        
-    // Проверяем наличие ошибок в массиве ошибок
-    $errorsNum = false;
-    foreach($errors as $es) {
-        if (is_array($es)) {
-            foreach($es as $e) {
-                if (isset($e)) {
-                    $errorsNum = TRUE;
-                }
-            }
-        } else {
-            if (isset($es)) {
-                    $errorsNum = TRUE;
-                }
-        }            
-    }        
-    // Если ошибок нет, то сохраняем студента в базу и делаем редиерект с параметром успешной регистрации        
-    if (!$errorsNum ) {        
+    $errors = $abiturientValidator->validate($abiturient);    
+    // Если ошибок нет, то обновляем или сохраняем студента в базу
+    // и делаем редиерект с параметром успешной регистрации        
+    if ($errors->isEmpty()) {        
         if ($userId) {
-           $adg->updateStudent($abiturient, $userId); // обновляем инфу зарегистрированного студента в базе
-        } else { // сохраняем нового студента в базу
-            // Генерируем пароль            
-            $userPassword = $autorization->generatePass();
-            $abiturient->setUserPassword($userPassword);            
-            $adg->addStudent($abiturient);
-            // Устанавливаем куки
-            setcookie ('userPassword', $userPassword, strtotime( '+10 years' ));           
+            // обновляем инфу зарегистрированного студента в базе
+           $adg->updateAbiturient($abiturient, $userId); 
+        } else {            
+            // Создаем новую регистрационную запись
+            $abiturient->userPassHash = $autorization->createNewRegistry();
+            // сохраняем нового студента в базу
+            $adg->addAbiturient($abiturient);           
         }           
         header('Location: index.php?notify=registered');            
     }
 } else {
     if ($userId) {
-        $values = $adg->getStudent($userId); 
+        // Загружаем студента вслучае удачной авторизации
+        $abiturient = $adg->getAbiturient($userId);        
     }
 }
 // Подгружаем страницу регистрации
-require ("../templates/header.html");
-require ("../templates/form.html");
+$view = new app\View;
+$view->render('form.html', [
+                            'abiturient' => $abiturient,
+                            'errors' => $errors   
+                           ]);
